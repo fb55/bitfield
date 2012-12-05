f950df8d18fa6549f9111a0ea28c428af09243de
Felix Böhm <me@feedic.com>
2012-12-05 10:46:16 +0100

    limit the length of the bitfield to what's actually needed
    otherwise, it would have used one byte more than needed for lengths

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 var Container = typeof Buffer !== "undefined" ? Buffer //in node, use buffers
-                : typeof Int8Array !== "undefined" ? Int8Array //in newer browsers, use webgl int8arrays
-                : function(l){ var a=new Array(l); for(var i=0;i<l;i++)a[i]=0; }; //else, do something similar
+		: typeof Int8Array !== "undefined" ? Int8Array //in newer browsers, use webgl int8arrays
+		: function(l){ var a=new Array(l); for(var i=0;i<l;i++)a[i]=0; }; //else, do something similar
 
 function BitField(data){
-        this.buffer = typeof data === "number" ? new Container(1 + (data >> 3)) : data;
+	if(typeof data === "number"){
+		if(data % 8 !== 0) data += 1 << 3;
+		data = new Container(data >> 3);
+	}
+	this.buffer = data;
 }
 
 BitField.prototype.get = function(i){
-        return !!(this.buffer[i >> 3] & (1 << (i % 8)));
+	return !!(this.buffer[i >> 3] & (1 << (i % 8)));
 };
 
 BitField.prototype.set = function(i, b){
-        if(b || arguments.length === 1){
-                this.buffer[i >> 3] |= 1 << (i % 8);
-        } else {
-                this.buffer[i >> 3] &= ~(1 << (i % 8));
-        }
+	if(b || arguments.length === 1){
+		this.buffer[i >> 3] |= 1 << (i % 8);
+	} else {
+		this.buffer[i >> 3] &= ~(1 << (i % 8));
+	}
 };
 
 if(typeof module !== "undefined") module.exports = BitField;
