51200d8327d5b91fa3f59d495a1c69afb1c801fb
Ward Segers <w@rdsegers.be>
2018-11-17 15:31:40 +0100

    data is not a number

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,6 @@ declare class Bitfield {
     constructor(data: number, options: any);
     get(i: number): boolean;
     set(i: number, b: boolean);
-    buffer: number;
+    buffer: Buffer | Uint8Array;
 }
 export = Bitfield;
