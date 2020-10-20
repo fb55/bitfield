0d6dc03e6ef4aeb36f00054d5b172dd207b16c2a
Felix Böhm <me@feedic.com>
2020-10-20 18:26:51 -0400

    feat: Port library to TypeScript
    * Also introduces ESlint, Prettier & Jest.
    * Adds missing tests for full coverage.
    * Adds JSDoc comments to all methods.

---
+++ src/index.spec.ts
@@ -0,0 +1,155 @@
+import BitField from "./";
+
+const data = "011011100110111".split("").map(Number).map(Boolean);
+
+describe("Bitfield", () => {
+    it("should be empty when initialized", () => {
+        const field = new BitField(data.length);
+
+        for (let i = 0; i < data.length; i++) {
+            expect(field.get(i)).toBe(false);
+        }
+    });
+
+    // Write data
+    it("should reproduce written data", () => {
+        const field = new BitField(data.length);
+
+        for (let i = 0; i < data.length; i++) {
+            field.set(i, data[i]);
+        }
+
+        for (let i = 0; i < data.length; i++) {
+            expect(field.get(i)).toBe(data[i]);
+        }
+    });
+
+    it("out-of-bounds should simply be false", () => {
+        const field = new BitField(data.length);
+
+        for (let i = data.length; i < 1e3; i++) {
+            expect(field.get(i)).toBe(false);
+        }
+    });
+
+    it("should not grow by default", () => {
+        const field = new BitField(data.length);
+        let index = 25;
+
+        for (let i = 0; i < 100; i++) {
+            index += 8 + Math.floor(32 * Math.random());
+
+            const oldLength = field.buffer.length;
+            expect(field.get(index)).toBe(false);
+
+            // Should not have grown for get()
+            expect(field.buffer).toHaveLength(oldLength);
+
+            field.set(index, true);
+
+            // Should not have grown for set()
+            expect(field.buffer).toHaveLength(oldLength);
+            expect(field.get(index)).toBe(false);
+        }
+    });
+
+    it("should be able to grow to infinity", () => {
+        const growField = new BitField(data.length, { grow: Infinity });
+        let index = 25;
+
+        for (let i = 0; i < 100; i++) {
+            index += 8 + Math.floor(32 * Math.random());
+
+            const oldLength = growField.buffer.length;
+            expect(growField.get(index)).toBe(false);
+            // Should not have grown for get()
+            expect(growField.buffer).toHaveLength(oldLength);
+            growField.set(index, true);
+            // Should have grown for set()
+            expect(growField.buffer.length).toBeGreaterThanOrEqual(
+                Math.ceil((index + 1) / 8)
+            ),
+                expect(growField.get(index)).toBe(true);
+        }
+    });
+
+    it("should restrict growth to growth option", () => {
+        const smallGrowField = new BitField(0, { grow: 50 });
+
+        for (let i = 0; i < 100; i++) {
+            const oldLength = smallGrowField.buffer.length;
+            smallGrowField.set(i, true);
+            if (i <= 55) {
+                // Should have grown for set()
+                expect(smallGrowField.buffer.length).toBeGreaterThanOrEqual(
+                    (i >> 3) + 1
+                );
+                expect(smallGrowField.get(i)).toBe(true);
+            } else {
+                // Should not have grown for set()
+                expect(smallGrowField.buffer).toHaveLength(oldLength);
+                expect(smallGrowField.get(i)).toBe(false);
+            }
+        }
+    });
+
+    it("should assume size 0 if no data or size passed in", () => {
+        const field2 = new BitField();
+        expect(field2.buffer).not.toBeNull();
+    });
+
+    it("should accept a typed array as input", () => {
+        const original = new BitField(0, { grow: 100 });
+        original.set(15);
+        const copy = new BitField(original.buffer);
+        expect(copy.get(15)).toBe(true);
+    });
+
+    it("should support disabling a field", () => {
+        const field = new BitField(0, { grow: 100 });
+        field.set(3, true);
+        expect(field.get(3)).toBe(true);
+        field.set(3, false);
+
+        // Check the first 10 indices, to ensure we only mutated a single field
+        for (let i = 0; i < 10; i++) {
+            expect(field.get(i)).toBe(false);
+        }
+
+        // Set the first 10 fields, then disable one
+        for (let i = 0; i < 10; i++) {
+            field.set(i);
+        }
+
+        field.set(5, false);
+        for (let i = 0; i < 10; i++) {
+            expect(field.get(i)).toBe(i !== 5);
+        }
+    });
+
+    it("should ignore disables out of bounds", () => {
+        const field = new BitField(0, { grow: 100 });
+        field.set(3, false);
+        expect(field.buffer).toHaveLength(0);
+    });
+
+    it("correct size bitfield", () => {
+        expect(new BitField(1).buffer).toHaveLength(1);
+        expect(new BitField(2).buffer).toHaveLength(1);
+        expect(new BitField(3).buffer).toHaveLength(1);
+        expect(new BitField(4).buffer).toHaveLength(1);
+        expect(new BitField(5).buffer).toHaveLength(1);
+        expect(new BitField(6).buffer).toHaveLength(1);
+        expect(new BitField(7).buffer).toHaveLength(1);
+        expect(new BitField(8).buffer).toHaveLength(1);
+        expect(new BitField(9).buffer).toHaveLength(2);
+        expect(new BitField(10).buffer).toHaveLength(2);
+        expect(new BitField(11).buffer).toHaveLength(2);
+        expect(new BitField(12).buffer).toHaveLength(2);
+        expect(new BitField(13).buffer).toHaveLength(2);
+        expect(new BitField(14).buffer).toHaveLength(2);
+        expect(new BitField(15).buffer).toHaveLength(2);
+        expect(new BitField(16).buffer).toHaveLength(2);
+        expect(new BitField(17).buffer).toHaveLength(3);
+    });
+});
---
+++ src/index.ts
@@ -0,0 +1,82 @@
+function getByteSize(num: number): number {
+    let out = num >> 3;
+    if (num % 8 !== 0) out++;
+    return out;
+}
+
+interface BitFieldOptions {
+    /**
+     * If you `set` an index that is out-of-bounds, the bitfield
+     * will automatically grow so that the bitfield is big enough
+     * to contain the given index, up to the given size (in bit).
+     *
+     * If you want the Bitfield to grow indefinitely, pass `Infinity`.
+     *
+     * @default 0.
+     */
+    grow?: number;
+}
+
+export default class BitField {
+    /**
+     * Grow the bitfield up to this number of entries.
+     * @default 0.
+     */
+    private readonly grow: number;
+    /** The internal storage of the bitfield. */
+    public buffer: Uint8Array;
+
+    /**
+     *
+     *
+     * @param data Either a number representing the maximum number of supported bytes, or an Int8Array.
+     * @param opts Options for the bitfield.
+     */
+    constructor(data: number | Uint8Array = 0, opts?: BitFieldOptions) {
+        const grow = opts?.grow;
+        this.grow = (grow && isFinite(grow) && getByteSize(grow)) || grow || 0;
+        this.buffer =
+            typeof data === "number" ? new Uint8Array(getByteSize(data)) : data;
+    }
+
+    /**
+     * Get a particular bit.
+     *
+     * @param i Bit index to retrieve.
+     * @returns A boolean indicating whether the `i`th bit is set.
+     */
+    get(i: number): boolean {
+        const j = i >> 3;
+        return j < this.buffer.length && !!(this.buffer[j] & (128 >> i % 8));
+    }
+
+    /**
+     * Set a particular bit.
+     *
+     * Will grow the underlying array if the bit is out of bounds and the `grow` option is set.
+     *
+     * @param i Bit index to set.
+     * @param value Value to set the bit to. Defaults to `true`.
+     */
+    set(i: number, value = true): void {
+        const j = i >> 3;
+        if (value) {
+            if (this.buffer.length < j + 1) {
+                const length = Math.max(
+                    j + 1,
+                    Math.min(2 * this.buffer.length, this.grow)
+                );
+                if (length <= this.grow) {
+                    const newBuffer = new Uint8Array(length);
+                    newBuffer.set(this.buffer);
+                    this.buffer = newBuffer;
+                }
+            }
+            // Set
+            this.buffer[j] |= 128 >> i % 8;
+        } else if (j < this.buffer.length) {
+            // Clear
+            this.buffer[j] &= ~(128 >> i % 8);
+        }
+    }
+}
