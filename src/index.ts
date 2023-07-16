c7a6997d3f744395bcc8a211ea36902d1cecd0b2
Felix Boehm <188768+fb55@users.noreply.github.com>
2023-07-16 13:03:57 +0100

    feat: Add `setAll` method, `length` prop (#661)
    Also improves the readability of the code by using full variable names.
    
    Restructures tests into groups.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-function getByteSize(num: number): number {
-    let out = num >> 3;
-    if (num % 8 !== 0) out++;
-    return out;
+/**
+ * Converts a number of bits to a number of bytes.
+ *
+ * @param numberOfBits The number of bits to convert.
+ * @returns The number of bytes that are needed to store the given number of bits.
+ */
+function bitsToBytes(numberOfBits: number): number {
+    return (numberOfBits >> 3) + Number(numberOfBits % 8 !== 0);
 }
 
 interface BitFieldOptions {
@@ -25,6 +29,10 @@ export default class BitField {
     private readonly grow: number;
     /** The internal storage of the bitfield. */
     public buffer: Uint8Array;
+    /** The number of bits in the bitfield. */
+    get length(): number {
+        return this.buffer.length >> 3;
+    }
 
     /**
      *
@@ -32,22 +40,29 @@ export default class BitField {
      * @param data Either a number representing the maximum number of supported bytes, or a Uint8Array.
      * @param opts Options for the bitfield.
      */
-    constructor(data: number | Uint8Array = 0, opts?: BitFieldOptions) {
-        const grow = opts?.grow;
-        this.grow = grow ? (isFinite(grow) ? getByteSize(grow) : grow) : 0;
+    constructor(data: number | Uint8Array = 0, options?: BitFieldOptions) {
+        const grow = options?.grow;
+        this.grow = grow
+            ? Number.isFinite(grow)
+                ? bitsToBytes(grow)
+                : grow
+            : 0;
         this.buffer =
-            typeof data === "number" ? new Uint8Array(getByteSize(data)) : data;
+            typeof data === "number" ? new Uint8Array(bitsToBytes(data)) : data;
     }
 
     /**
      * Get a particular bit.
      *
-     * @param i Bit index to retrieve.
+     * @param bitIndex Bit index to retrieve.
      * @returns A boolean indicating whether the `i`th bit is set.
      */
-    get(i: number): boolean {
-        const j = i >> 3;
-        return j < this.buffer.length && !!(this.buffer[j] & (128 >> i % 8));
+    get(bitIndex: number): boolean {
+        const byteIndex = bitIndex >> 3;
+        return (
+            byteIndex < this.buffer.length &&
+            !!(this.buffer[byteIndex] & (0b1000_0000 >> bitIndex % 8))
+        );
     }
 
     /**
@@ -55,15 +70,15 @@ export default class BitField {
      *
      * Will grow the underlying array if the bit is out of bounds and the `grow` option is set.
      *
-     * @param i Bit index to set.
+     * @param bitIndex Bit index to set.
      * @param value Value to set the bit to. Defaults to `true`.
      */
-    set(i: number, value = true): void {
-        const j = i >> 3;
+    set(bitIndex: number, value = true): void {
+        const byteIndex = bitIndex >> 3;
         if (value) {
-            if (this.buffer.length < j + 1) {
+            if (this.buffer.length < byteIndex + 1) {
                 const length = Math.max(
-                    j + 1,
+                    byteIndex + 1,
                     Math.min(2 * this.buffer.length, this.grow),
                 );
                 if (length <= this.grow) {
@@ -73,33 +88,78 @@ export default class BitField {
                 }
             }
             // Set
-            this.buffer[j] |= 128 >> i % 8;
-        } else if (j < this.buffer.length) {
+            this.buffer[byteIndex] |= 0b1000_0000 >> bitIndex % 8;
+        } else if (byteIndex < this.buffer.length) {
             // Clear
-            this.buffer[j] &= ~(128 >> i % 8);
+            this.buffer[byteIndex] &= ~(0b1000_0000 >> bitIndex % 8);
+        }
+    }
+
+    /**
+     * Sets a value or an array of values.
+     *
+     * @param array An array of booleans to set.
+     * @param offset The bit offset at which the values are to be written.
+     */
+    setAll(array: ArrayLike<boolean>, offset = 0): void {
+        const targetLength = Math.min(
+            bitsToBytes(offset + array.length),
+            this.grow,
+        );
+
+        if (this.buffer.length < targetLength) {
+            const newBuffer = new Uint8Array(targetLength);
+            newBuffer.set(this.buffer);
+            this.buffer = newBuffer;
+        }
+
+        let byteIndex = offset >> 3;
+        let bitMask = 0b1000_0000 >> offset % 8;
+        for (let index = 0; index < array.length; index++) {
+            if (array[index]) {
+                this.buffer[byteIndex] |= bitMask;
+            } else {
+                this.buffer[byteIndex] &= ~bitMask;
+            }
+
+            if (bitMask === 1) {
+                byteIndex += 1;
+
+                if (byteIndex >= this.buffer.length) {
+                    break;
+                }
+
+                bitMask = 0b1000_0000;
+            } else {
+                bitMask >>= 1;
+            }
         }
     }
 
     /**
      * Loop through the bits in the bitfield.
      *
-     * @param fn Function to be called with the bit value and index.
+     * @param callbackfn Function to be called with the bit value and index.
      * @param start Index of the first bit to look at.
      * @param end Index of the first bit that should no longer be considered.
      */
     forEach(
-        fn: (bit: boolean, index: number) => void,
+        callbackfn: (bit: boolean, index: number) => void,
         start = 0,
         end = this.buffer.length * 8,
     ): void {
-        for (
-            let i = start, j = i >> 3, y = 128 >> i % 8, byte = this.buffer[j];
-            i < end;
-            i++
-        ) {
-            fn(!!(byte & y), i);
+        let byteIndex = start >> 3;
+        let bitMask = 0b1000_0000 >> start % 8;
 
-            y = y === 1 ? ((byte = this.buffer[++j]), 128) : y >> 1;
+        for (let bitIndex = start; bitIndex < end; bitIndex++) {
+            callbackfn(!!(this.buffer[byteIndex] & bitMask), bitIndex);
+
+            if (bitMask === 1) {
+                byteIndex += 1;
+                bitMask = 0b1000_0000;
+            } else {
+                bitMask >>= 1;
+            }
         }
     }
 }
