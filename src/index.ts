fe5109974d572a6509a3bf5b456cda9b0cf7694c
Felix <188768+fb55@users.noreply.github.com>
2023-07-16 12:57:18 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -146,7 +146,7 @@ export default class BitField {
     forEach(
         callbackfn: (bit: boolean, index: number) => void,
         start = 0,
-        end = this.buffer.length * 8,
+        end = this.buffer.length >> 3,
     ): void {
         let byteIndex = start >> 3;
         let bitMask = 0b1000_0000 >> start % 8;
